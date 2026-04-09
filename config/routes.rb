e2a5f2bab6a10eb1d15c209c15770921e3c31883
kawa8080 <R.riverJob1590@gmail.com>
2026-04-09 22:44:03 +0900

    Todo一覧画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "up" => "rails/health#show", as: :rails_health_check
-  # root "posts#index"
+  resources :todos, only: [:index]
+  root "todos#index"
 end
