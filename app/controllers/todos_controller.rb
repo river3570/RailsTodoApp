1cd4872567cb1a692e2a8a71e820f8755017803e
river3570 <R.riverJob1590@gmail.com>
2026-04-14 14:13:30 +0900

    Todo新規作成機能の追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,6 +4,20 @@ class TodosController < ApplicationController
     @todos = current_user.todos
   end
 
+  def new
+    @todo = current_user.todos.build
+  end
+
+  def create
+    @todo = current_user.todos.build(todo_params)
+
+    if @todo.save
+      redirect_to todos_path, notice: "作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     todo = current_user.todos.find(params[:id])
 
@@ -12,5 +26,11 @@ class TodosController < ApplicationController
     else
       redirect_to todos_path, notice: "削除に失敗しました"
     end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :context)
   end
 end
